1f93f0b0bd39ee33e9dbbe4cea3e9ce4aff762cc
Paul Huang <paul_hzq@hotmail.com>
2017-08-05 23:14:37 -0400

    create the homepage

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Post,Comment,Author
 # Create your views here.
+
+def index(request):
+    """
+    View function for home page of site.
+    """
+    #
+    return render(request,'index.html',context={})
