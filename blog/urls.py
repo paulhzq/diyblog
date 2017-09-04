46d76e184d8ed637b3a71289c0f9acbb80a9ad65
Paul Huang <paul_hzq@hotmail.com>
2017-09-03 22:36:59 -0400

    add blog comment form

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,8 @@ from . import views
 urlpatterns = [
     url(r'^$',views.index, name='index'),
     url(r'^blogs/?$',views.BlogListView.as_view(), name='blogs'),
-    url(r'^blogs/(?P<pk>\d+)$',views.BlogDetailView.as_view(), name='blog-detail'),
+    url(r'^blog/(?P<pk>\d+)$',views.BlogDetailView.as_view(), name='blog-detail'),
     url(r'bloggers/$',views.BloggerListView.as_view(), name='bloggers'),
     url(r'bloggers/(?P<pk>\d+)$', views.BloggerDetailView.as_view(), name='blogger-detail'),
+    url(r'^blog/(?P<pk>\d+)/comment/$', views.BlogCommentCreate.as_view(), name='blog_comment'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -33,3 +33,41 @@ class BloggerDetailView(generic.ListView):
         context = super(BloggerDetailView, self).get_context_data(**kwargs)
         context['author'] = get_object_or_404(Author,pk = self.kwargs['pk'])
         return context
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
+from django.urls import reverse
+
+class BlogCommentCreate(LoginRequiredMixin, CreateView):
+    """
+    Form for adding a blog comment. Requires login.
+    """
+    model = Comment
+    fields = ['content',]
+
+    def get_context_data(self, **kwargs):
+        """
+        Add associated blog to form template so can display its title in HTML.
+        """
+        # Call the base implementation first to get a context
+        context = super(BlogCommentCreate, self).get_context_data(**kwargs)
+        # Get the blog from id and add it to the context
+        context['blog'] = get_object_or_404(Post, pk = self.kwargs['pk'])
+        return context
+
+    def form_valid(self, form):
+        """
+        Add author and associated blog to form data before setting it as valid (so it is saved to model)
+        """
+        #Add logged-in user as author of comment
+        form.instance.author = self.request.user
+        #Associate comment with blog based on passed id
+        form.instance.blog=get_object_or_404(Post, pk = self.kwargs['pk'])
+        # Call super-class form validation behaviour
+        return super(BlogCommentCreate, self).form_valid(form)
+
+    def get_success_url(self):
+        """
+        After posting comment return to associated blog.
+        """
+        return reverse('blog-detail', kwargs={'pk': self.kwargs['pk'],})
